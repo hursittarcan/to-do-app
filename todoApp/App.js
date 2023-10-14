a55c585724e08ee094c26a80f197621b183f660c
hursittarcan <hursittarcan@gmail.com>
2023-10-14 12:36:50 +0200

    import Task component in App

--- todoApp/App.js
+++ todoApp/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from "./components/Task";
 
 export default function App() {
   return (
@@ -30,5 +31,5 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold'
   },
-  
+
 });
