518d682888af05a88897c52b922b41eab166d8ff
hursittarcan <hursittarcan@gmail.com>
2023-10-14 13:19:16 +0200

    add styles for 'add task' section

--- todoApp/App.js
+++ todoApp/App.js
@@ -42,5 +42,32 @@ const styles = StyleSheet.create({
   },
   items: {
     marginTop: 30
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    paddingVertical: 15,
+    width: 250,
+    paddingHorizontal: 15,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1
   }
 });
